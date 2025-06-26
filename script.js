c9bf92491b5a4acdb40543178bbee7c8807c257e
thiago0e <thiago.thuler.pontes@escola.pr.gov.br>
2025-06-26 11:42:41 +0000

    26/06

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => { 
     button.addEventListener('click', function (){
-        
+    const atual = document.querySelector('.ativo');
+    const proximoPasso = 'passo-' + this.getAttri
     })
 })
